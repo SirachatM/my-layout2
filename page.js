7cab392ef9d380a63fe0f070ffdd27fd24594f54
9Nikeeeeeeeee <destinychadonike@gmail.com>
2024-07-01 13:49:38 +0700

    first commit

---
+++ page.js
@@ -0,0 +1,30 @@
+export default function Home() {
+  return (
+<>
+      <div className="container-fruid">
+
+        <div className="row">
+          <div className="col-12 text-center bg-primary p-5">
+            HEADER
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6 text-center bg-warning p-5">
+            CONTEN1
+          </div>
+          <div className="col-6 text-center bg-success p-5">
+            CONTEN2
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12 text-center bg-info p-5">
+            FOOTER
+          </div>
+        </div>
+
+      </div>
+</>
+  );
+}
